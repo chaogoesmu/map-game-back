1bc53f626a984e40cb4aa0b8b12ed598495bcc32
Cullen <chaogoesmu@gmail.com>
2018-04-16 18:14:36 -0700

    fixed a bug with third migration

--- migrations/20180416171218_first.js
+++ migrations/20180416171218_first.js
@@ -13,7 +13,7 @@ exports.up = function(knex, Promise) {
 };
 
 exports.down = function(knex, Promise) {
-
+  return knex.schema.dropTable('users');
 };
 /*
 CREATE TABLE users (
--- migrations/20180416172327_third.js
+++ migrations/20180416172327_third.js
@@ -1,7 +1,7 @@
 
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('usersingames', table => {
-    table.increments('id').primary()
+    table.integer('gid')
     table.foreign('gid').references('games.id').onDelete('CASCADE')//on delete cascade?
     table.integer('pid').unique()
     table.float('lat').notNullable().defaultsTo(0)
@@ -11,12 +11,12 @@ exports.up = function(knex, Promise) {
 };
 
 exports.down = function(knex, Promise) {
-
+  return knex.schema.dropTable('usersingames');
 };
 /*
 CREATE TABLE usersingames(
   gid   INT REFERENCES games(id) ON DELETE CASCADE,
-  pid   INT UNIQUE
+  pid   INT UNIQUE,
   lat   FLOAT,
   long  FLOAT,
   captured  BOOL
